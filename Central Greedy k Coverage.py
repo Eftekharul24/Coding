f04b64d2319f5d0dbf6bf050a855d792e56fbda5
MD. Eftekharul Islam <144184559+Eftekharul24@users.noreply.github.com>
2023-09-17 07:05:57 +0600

    Update Central Greedy k Coverage.py

--- Central Greedy k Coverage.py
+++ Central Greedy k Coverage.py
@@ -4,34 +4,51 @@ import math
 def distance(point1, point2):
     return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
 
-# Function to find the sensor with maximum uncovered area
-def find_max_uncovered_sensor(sensors, covered_area):
-    max_uncovered_sensor = None
-    max_uncovered_area = 0
+# Function to calculate the utility of a sensor based on incentive values
+def calculate_utility(sensor, incentive_values):
+    utility = 0
+    for i in range(len(sensor['coverage'])):
+        utility += incentive_values[i] * sensor['coverage'][i]
+    return utility
 
-    for sensor in sensors:
-        uncovered_area = 0
-        for point in sensor['coverage']:
-            if point not in covered_area:
-                uncovered_area += 1
-        if uncovered_area > max_uncovered_area:
-            max_uncovered_area = uncovered_area
-            max_uncovered_sensor = sensor
-
-    return max_uncovered_sensor
-
-# Central Greedy k-Coverage Algorithm
-def central_greedy_k_coverage(sensors, k):
+# Function to select k sensors using the Greedy Linear algorithm
+def greedy_linear(sensors, k, incentive_values):
     selected_sensors = []
-    covered_area = set()
 
     while len(selected_sensors) < k:
-        max_uncovered_sensor = find_max_uncovered_sensor(sensors, covered_area)
-        if max_uncovered_sensor is None:
-            break
-        selected_sensors.append(max_uncovered_sensor)
-        for point in max_uncovered_sensor['coverage']:
-            covered_area.add(point)
+        max_utility_sensor = None
+        max_utility = -float('inf')
+
+        for sensor in sensors:
+            if sensor not in selected_sensors:
+                utility = calculate_utility(sensor, incentive_values)
+                if utility > max_utility:
+                    max_utility = utility
+                    max_utility_sensor = sensor
+
+        if max_utility_sensor is not None:
+            selected_sensors.append(max_utility_sensor)
+
+    return selected_sensors
+
+# Function to select k sensors using the Greedy Quadratic algorithm
+def greedy_quadratic(sensors, k, incentive_values):
+    selected_sensors = []
+
+    while len(selected_sensors) < k:
+        max_utility_sensor = None
+        max_utility = -float('inf')
+
+        for sensor in sensors:
+            if sensor not in selected_sensors:
+                utility = calculate_utility(sensor, incentive_values)
+                if utility > max_utility:
+                    max_utility = utility
+                    max_utility_sensor = sensor
+
+        if max_utility_sensor is not None:
+            selected_sensors.append(max_utility_sensor)
+            incentive_values = [incentive_values[i] / 2 for i in range(len(incentive_values))]
 
     return selected_sensors
 
@@ -39,16 +56,26 @@ def central_greedy_k_coverage(sensors, k):
 if __name__ == "__main__":
     # Define sensor nodes with their positions and coverage areas
     sensors = [
-        {'position': (0, 0), 'coverage': [(0, 0), (1, 0), (0, 1)]},
-        {'position': (1, 1), 'coverage': [(1, 1), (2, 1), (1, 2)]},
-        {'position': (2, 0), 'coverage': [(2, 0), (3, 0), (2, 1)]},
-        {'position': (0, 2), 'coverage': [(0, 2), (1, 2), (0, 3)]}
+        {'position': (0, 0), 'coverage': [1, 0, 0, 0]},
+        {'position': (1, 1), 'coverage': [0, 1, 0, 0]},
+        {'position': (2, 0), 'coverage': [0, 0, 1, 0]},
+        {'position': (0, 2), 'coverage': [0, 0, 0, 1]}
     ]
 
-    k = 2  # Number of sensors to select
-    selected_sensors = central_greedy_k_coverage(sensors, k)
+    k = 3  # Number of sensors to select
 
-    print("Selected sensors:")
-    for sensor in selected_sensors:
+    incentive_values_linear = [1, 1, 1, 0]
+    selected_sensors_linear = greedy_linear(sensors, k, incentive_values_linear)
+
+    print("Greedy Linear selected sensors:")
+    for sensor in selected_sensors_linear:
         print(f"Sensor at {sensor['position']}")
+
+    incentive_values_quadratic = [5, 3, 1, 0]
+    selected_sensors_quadratic = greedy_quadratic(sensors, k, incentive_values_quadratic)
+
+    print("\nGreedy Quadratic selected sensors:")
+    for sensor in selected_sensors_quadratic:
+        print(f"Sensor at {sensor['position']}")
+
 
